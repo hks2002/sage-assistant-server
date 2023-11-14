275e4e5edcf6e04d301755352f4c8c2c3baa2078
Robert Huang <56649783@qq.com>
2023-11-14 17:15:56 +0800

    feat: ✨add duplicated PO function

--- src/main/java/com/da/sageassistantserver/dao/DirtyDataMapper.java
+++ src/main/java/com/da/sageassistantserver/dao/DirtyDataMapper.java
@@ -1,11 +1,10 @@
-/*********************************************************************************************************************
- * @Author                : Robert Huang<56649783@qq.com>                                                            *
- * @CreatedDate           : 2022-03-26 17:55:00                                                                      *
- * @LastEditors           : Robert Huang<56649783@qq.com>                                                            *
- * @LastEditDate          : 2023-03-12 19:27:23                                                                      *
- * @FilePath              : src/main/java/sageassistant/dataSrv/dao/CommonMapper.java                                *
- * @CopyRight             : Dedienne Aerospace China ZhuHai                                                          *
- ********************************************************************************************************************/
+/*****************************************************************************
+ * @Author                : Robert Huang<56649783@qq.com>                    *
+ * @CreatedDate           : 2022-03-26 17:55:00                              *
+ * @LastEditors           : Robert Huang<56649783@qq.com>                    *
+ * @LastEditDate          : 2023-11-14 17:50:21                              *
+ * @CopyRight             : Dedienne Aerospace China ZhuHai                  *
+ ****************************************************************************/
 
 package com.da.sageassistantserver.dao;
 
@@ -19,11 +18,11 @@ import com.da.sageassistantserver.model.DirtyDataDuplicatedRA;
 
 @Mapper
 public interface DirtyDataMapper {
-    List<DirtyDataDuplicatedPO> findDuplicatedInterPOBySite(@Param("Site") String Site);
+        List<DirtyDataDuplicatedPO> findDuplicatedInterPOBySite(@Param("Site") String Site);
 
-    List<DirtyDataDuplicatedPO> findDuplicatedOuterPOBySite(@Param("Site") String Site,
-            @Param("FromDate") String FromDate);
+        List<DirtyDataDuplicatedPO> findDuplicatedOuterPOBySite(@Param("Site") String Site,
+                        @Param("DateFrom") String DateFrom);
 
-    List<DirtyDataDuplicatedRA> findDuplicatedRABySite(@Param("Site") String Site,
-            @Param("FromDate") String FromDate);
+        List<DirtyDataDuplicatedRA> findDuplicatedRABySite(@Param("Site") String Site,
+                        @Param("DateFrom") String DateFrom);
 }
