0c7a1e0443b0d2ba23edf0e88bc90b2728cc93ac
Robert Huang <56649783@qq.com>
2023-11-15 14:49:49 +0800

    fix: 🐛fix duplicated po

--- src/main/java/com/da/sageassistantserver/service/DirtyDataService.java
+++ src/main/java/com/da/sageassistantserver/service/DirtyDataService.java
@@ -1,11 +1,10 @@
-/*********************************************************************************************************************
- * @Author                : Robert Huang<56649783@qq.com>                                                            *
- * @CreatedDate           : 2022-03-26 17:57:00                                                                      *
- * @LastEditors           : Robert Huang<56649783@qq.com>                                                            *
- * @LastEditDate          : 2023-06-24 16:03:39                                                                      *
- * @FilePath              : src/main/java/com/da/sageassistantserver/service/StatusService.java                      *
- * @CopyRight             : Dedienne Aerospace China ZhuHai                                                          *
- ********************************************************************************************************************/
+/******************************************************************************
+ * @Author                : Robert Huang<56649783@qq.com>                     *
+ * @CreatedDate           : 2022-03-26 17:57:00                               *
+ * @LastEditors           : Robert Huang<56649783@qq.com>                     *
+ * @LastEditDate          : 2023-11-15 21:04:23                               *
+ * @CopyRight             : Dedienne Aerospace China ZhuHai                   *
+ *****************************************************************************/
 
 package com.da.sageassistantserver.service;
 
@@ -24,20 +23,14 @@ public class DirtyDataService {
     @Autowired
     private DirtyDataMapper dirtyDataMapper;
 
-    public List<DirtyDataDuplicatedPO> findDuplicatedInterPOBySite(String Site) {
-        List<DirtyDataDuplicatedPO> listPage = dirtyDataMapper.findDuplicatedInterPOBySite(Site);
+    public List<DirtyDataDuplicatedPO> findDuplicatedPOBySite(String Site, String DateFrom, String OnlyForSales) {
+        List<DirtyDataDuplicatedPO> listPage = dirtyDataMapper.findDuplicatedPOBySite(Site, DateFrom, OnlyForSales);
 
         return listPage;
     }
 
-    public List<DirtyDataDuplicatedPO> findDuplicatedOuterPOBySite(String Site, String DateFrom) {
-        List<DirtyDataDuplicatedPO> listPage = dirtyDataMapper.findDuplicatedOuterPOBySite(Site, DateFrom);
-
-        return listPage;
-    }
-
-    public List<DirtyDataDuplicatedRA> findDuplicatedRABySite(String Site, String DateFrom) {
-        List<DirtyDataDuplicatedRA> listPage = dirtyDataMapper.findDuplicatedRABySite(Site, DateFrom);
+    public List<DirtyDataDuplicatedRA> findDuplicatedRABySite(String Site, String DateFrom, String OnlyForSales) {
+        List<DirtyDataDuplicatedRA> listPage = dirtyDataMapper.findDuplicatedRABySite(Site, DateFrom, OnlyForSales);
 
         return listPage;
     }
