f3469ac1367e6fb0bb604634a90bc3292afb5b1b
Robert Huang <56649783@qq.com>
2023-11-15 14:49:49 +0800

    fix: 🐛fix duplicated po

--- src/main/java/com/da/sageassistantserver/model/DirtyDataDuplicatedRA.java
+++ src/main/java/com/da/sageassistantserver/model/DirtyDataDuplicatedRA.java
@@ -1,11 +1,10 @@
-/*********************************************************************************************************************
- * @Author                : Robert Huang<56649783@qq.com>                                                            *
- * @CreatedDate           : 2022-09-21 09:17:00                                                                      *
- * @LastEditors           : Robert Huang<56649783@qq.com>                                                            *
- * @LastEditDate          : 2023-06-24 15:51:57                                                                      *
- * @FilePath              : src/main/java/com/da/sageassistantserver/model/AnalysesPurchase.java                     *
- * @CopyRight             : Dedienne Aerospace China ZhuHai                                                          *
- ********************************************************************************************************************/
+/******************************************************************************
+ * @Author                : Robert Huang<56649783@qq.com>                     *
+ * @CreatedDate           : 2022-09-21 09:17:00                               *
+ * @LastEditors           : Robert Huang<56649783@qq.com>                     *
+ * @LastEditDate          : 2023-11-14 19:28:24                               *
+ * @CopyRight             : Dedienne Aerospace China ZhuHai                   *
+ *****************************************************************************/
 
 package com.da.sageassistantserver.model;
 
@@ -23,4 +22,5 @@ public class DirtyDataDuplicatedRA {
     private Integer ReceiptQty;
     private Float ReceiptAmount;
     private String Currency;
+    private Integer Req;
 }
