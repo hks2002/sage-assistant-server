a6fd233bf6c63e74c77871999ee98adbbb6faf1c
Robert Huang <56649783@qq.com>
2023-11-17 13:10:10 +0800

    refactor: 📦rename dirty data to suspect duplicate

---
+++ src/main/java/com/da/sageassistantserver/service/SuspectDuplicateDataService.java
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * @Author                : Robert Huang<56649783@qq.com>                     *
+ * @CreatedDate           : 2022-03-26 17:57:00                               *
+ * @LastEditors           : Robert Huang<56649783@qq.com>                     *
+ * @LastEditDate          : 2023-11-17 13:06:21                               *
+ * @CopyRight             : Dedienne Aerospace China ZhuHai                   *
+ *****************************************************************************/
+
+package com.da.sageassistantserver.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.da.sageassistantserver.dao.SuspectDuplicateDataMapper;
+import com.da.sageassistantserver.model.SuspectDuplicatedPO;
+import com.da.sageassistantserver.model.SuspectDuplicatedRA;
+
+@Service
+public class SuspectDuplicateDataService {
+
+    @Autowired
+    private SuspectDuplicateDataMapper dirtyDataMapper;
+
+    public List<SuspectDuplicatedPO> findDuplicatedPOBySite(String Site, String DateFrom, String OnlyForSales) {
+        List<SuspectDuplicatedPO> listPage = dirtyDataMapper.findDuplicatedPOBySite(Site, DateFrom, OnlyForSales);
+
+        return listPage;
+    }
+
+    public List<SuspectDuplicatedRA> findDuplicatedRABySite(String Site, String DateFrom, String OnlyForSales) {
+        List<SuspectDuplicatedRA> listPage = dirtyDataMapper.findDuplicatedRABySite(Site, DateFrom, OnlyForSales);
+
+        return listPage;
+    }
+
+}
