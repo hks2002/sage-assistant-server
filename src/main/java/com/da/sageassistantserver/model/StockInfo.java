2a82065c7dcb983edaa203902dea02e9216f51ed
Robert Huang <56649783@qq.com>
2024-03-19 17:37:04 +0800

    feat: ✨PNOGSEM Inventory Info

--- src/main/java/com/da/sageassistantserver/model/StockInfo.java
+++ src/main/java/com/da/sageassistantserver/model/StockInfo.java
@@ -1,16 +1,14 @@
-/*********************************************************************************************************************
- * @Author                : Robert Huang<56649783@qq.com>                                                            *
- * @CreatedDate           : 2022-03-26 17:01:00                                                                      *
- * @LastEditors           : Robert Huang<56649783@qq.com>                                                            *
- * @LastEditDate          : 2023-03-12 13:21:45                                                                      *
- * @FilePath              : src/main/java/sageassistant/dataSrv/model/StockInfo.java                                 *
- * @CopyRight             : Dedienne Aerospace China ZhuHai                                                          *
- ********************************************************************************************************************/
+/******************************************************************************
+ * @Author                : Robert Huang<56649783@qq.com>                     *
+ * @CreatedDate           : 2022-03-26 17:01:00                               *
+ * @LastEditors           : Robert Huang<56649783@qq.com>                     *
+ * @LastEditDate          : 2024-03-19 17:24:33                               *
+ * @CopyRight             : Dedienne Aerospace China ZhuHai                   *
+ *****************************************************************************/
 
 package com.da.sageassistantserver.model;
 
 import lombok.Data;
-
 
 @Data
 public class StockInfo {
@@ -18,4 +16,5 @@ public class StockInfo {
     private String StockSite;
     private String PN;
     private Integer Qty;
+    private String Type;
 }
