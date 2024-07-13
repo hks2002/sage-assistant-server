e27e72e2f86ebe9ea0c2e3169735df7561b32e75
Robert Huang <56649783@qq.com>
2024-07-13 23:24:35 +0800

    feat: ✨add auth filter

---
+++ src/main/java/com/da/sageassistantserver/service/SageActionService.java
@@ -0,0 +1,256 @@
+/******************************************************************************
+ * @Author                : Robert Huang<56649783@qq.com>                     *
+ * @CreatedDate           : 2022-11-23 20:45:00                               *
+ * @LastEditors           : Robert Huang<56649783@qq.com>                     *
+ * @LastEditDate          : 2024-06-07 18:54:40                               *
+ * @CopyRight             : Dedienne Aerospace China ZhuHai                   *
+ *****************************************************************************/
+
+package com.da.sageassistantserver.service;
+
+import java.net.http.HttpResponse;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.da.sageassistantserver.utils.SageActionHelper;
+import com.da.sageassistantserver.utils.SageActionHelper.MsgTyp;
+import com.microsoft.sqlserver.jdbc.StringUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class SageActionService {
+    /**
+     * contains `phase`, `Tracking`, and status is 202
+     */
+    public static boolean isPerformanceLow(JSONObject json) {
+        // only adjust body is enough
+        if (json.containsKey("phase") && json.getString("phase").equals("Tracking")) {
+            log.warn("Sage Server performance low");
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * In case of server performance low, sleep 1s, then get response by GET
+     * 
+     * @param auth
+     * @param location without schemas and hosts
+     * @return
+     */
+    public static JSONObject getTrackingResponse(String auth, String location) {
+        log.debug(location);
+
+        // Sleep 0.5 seconds
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String html = HttpService.request("https://192.168.10.62" + location, "GET", null, auth).body();
+        JSONObject json = JSONObject.parseObject(html);
+        return isPerformanceLow(json) ? getTrackingResponse(auth, location) : json;
+    }
+
+    public static JSONObject doSageAct(
+            String auth,
+            String function,
+            String trans,
+            String session,
+            String data,
+            String preTacking) {
+        String uuid = preTacking != null ? preTacking : UUID.randomUUID().toString();
+        HttpResponse<String> response = HttpService.request(
+                String.format(
+                        "https://192.168.10.62/trans/x3/erp/EXPLOIT/$sessions('%s')/requestSvc?act=%s&trackngId=%s",
+                        session,
+                        JSONObject.parseObject(data).getString("act"),
+                        uuid),
+                "PUT",
+                data,
+                auth);
+        String html = response.body();
+        JSONObject json = JSONObject.parseObject(html);
+
+        // Sage Server performance low, header contains `location`,
+        // Using GET to get the response by `location`, continue
+        if (isPerformanceLow(json)) {
+            String location = response.headers().firstValue("location").orElse("");
+            json = getTrackingResponse(auth, location);
+        }
+
+        JSONObject sap = json.getJSONObject("sap");
+        JSONObject target = sap != null ? sap.getJSONObject("target") : null;
+
+        JSONObject rtn = new JSONObject();
+
+        if (target != null) {
+            String targetType = target.getString("type");
+
+            switch (targetType) {
+                case "ist":
+                    // ist means success, without pop window
+                    rtn = SageActionHelper.rtnObj(true, MsgTyp.RESULT, "success");
+                    rtn.put("result", Optional.ofNullable(json).orElse(new JSONObject()).toJSONString());
+                    break;
+                case "box":
+                    JSONObject box = target.getJSONObject("box");
+                    Integer boxType = box.getInteger("type");
+
+                    switch (boxType) {
+                        case 1:
+                            // type 1 means info
+                            rtn = SageActionHelper.rtnObj(true, MsgTyp.INFO, box.getString("li"));
+                            break;
+                        case 2:
+                            // 2 means success, with pop window, need to select
+                            rtn = SageActionHelper.rtnObj(
+                                    true,
+                                    MsgTyp.QUESTION,
+                                    Optional.ofNullable(json).orElse(new JSONObject()).toJSONString());
+
+                            break;
+                        case 3:
+                            // 3 means warning
+                            rtn = SageActionHelper.rtnObj(false, MsgTyp.WARN, box.getString("li"));
+                            break;
+                        default:
+                            rtn = SageActionHelper.rtnObj(false, MsgTyp.ERROR, "Unknown response");
+                            break;
+                    }
+                    break;
+                case "portal":
+                    // portal means login expired or exit page
+                    if (Optional.ofNullable(sap).orElse(new JSONObject()).containsKey("func") &&
+                            Optional.ofNullable(sap).orElse(new JSONObject()).getJSONObject("func")
+                                    .containsKey("close")) {
+                        rtn = SageActionHelper.rtnObj(true, MsgTyp.INFO, "Page exit");
+                    } else {
+                        rtn = SageActionHelper.rtnObj(true, MsgTyp.ERROR, "Session expired");
+                    }
+                    break;
+                default:
+                    rtn = SageActionHelper.rtnObj(false, MsgTyp.ERROR, "Unknown response");
+            }
+            // } else if (srvop != null && diagnosis != null) {
+            // rtn = SageActionHelper.rtnObj(false, MsgTyp.ERROR,
+            // diagnosis.getString("$message"));
+        } else if (json.containsKey("$message")) {
+            log.debug(Optional.ofNullable(json.getString("$details")).orElse("Unknown response"));
+            rtn = SageActionHelper.rtnObj(false, MsgTyp.ERROR, json.getString("$message"));
+        } else {
+            rtn = SageActionHelper.rtnObj(false, MsgTyp.ERROR, "Unknown response");
+        }
+
+        return rtn;
+    }
+
+    public static JSONObject updateSageField(
+            String auth,
+            String targetMain,
+            String targetMainNO,
+            Integer modLine,
+            String targetSub,
+            String targetSubVal) {
+        String function = SageActionHelper.getFunction(targetMain);
+
+        // trans
+        String trans = switch (function) {
+            case "GESSOH" -> SageActionHelper.getSalesOrderTransaction(targetMainNO);
+            case "GESSIH" -> SageActionHelper.getInvoiceTransaction(targetMainNO);
+            case "GESPOH" -> SageActionHelper.getPurchaseTransaction(targetMainNO);
+            default -> "";
+        };
+
+        JSONObject session = SageLoginService.getSageSessionCache(auth, function, trans);
+        // if is not successful, return it
+        if (!session.getBooleanValue("success")) {
+            return session;
+        }
+
+        String sessionId = session.getString("SessionId");
+        String targetMainField = session.getString("xid");
+        String rcdNO = session.getString("rcdNO");
+
+        // step one: go to target recorder
+        log.debug("go to target recorder >>>");
+        JSONObject rtn = SageActionService.doSageAct(
+                auth,
+                function,
+                trans,
+                sessionId,
+                SageActionHelper.set("B", targetMainField, 0, targetMainNO),
+                null);
+        if (!rtn.getBooleanValue("success")) {
+            if (rtn.getString("msgTyp").equals("WARN")) {
+                // rewrite message, original message is ""
+                rtn.put("msg", "Recorder not found");
+            }
+            return rtn;
+        }
+        log.debug("go to target recorder <<<");
+
+        // step two: try to edit
+        log.debug("edit model >>>");
+        rtn = SageActionService.doSageAct(auth, function, trans, sessionId,
+                SageActionHelper.goTo("B", targetSub, modLine), null);
+        if (!rtn.getBooleanValue("success")) {
+            return rtn;
+        }
+        log.debug("edit model <<<");
+
+        // step three: always no revise
+        if (rtn.getBooleanValue("success") && rtn.getString("msgTyp").equals("QUESTION")) {
+            log.debug("always no revise >>>");
+            rtn = SageActionService.doSageAct(auth, function, trans, sessionId, SageActionHelper.noRevise(), null);
+            log.debug("always no revise <<<");
+        }
+
+        // step four: modify and save, based on the value type
+        // If someone else opened target, edit mode will return false, step four will be
+        // skipped.
+        if (rtn.getBooleanValue("success")) {
+            Integer iVal = -1;
+
+            if (StringUtils.isInteger(targetSubVal)) {
+                iVal = Integer.valueOf(targetSubVal);
+            }
+
+            log.debug("modify and save >>>");
+            // ❗❗❗❗❗ Limit integer value, assume that the value is list option val
+            if (iVal > 0 && iVal < 10) {
+                rtn = SageActionService.doSageAct(
+                        auth,
+                        function,
+                        trans,
+                        sessionId,
+                        SageActionHelper.save("B", targetSub, modLine, iVal),
+                        null);
+            } else {
+                rtn = SageActionService.doSageAct(
+                        auth,
+                        function,
+                        trans,
+                        sessionId,
+                        SageActionHelper.save("B", targetSub, modLine, targetSubVal),
+                        null);
+            }
+            log.debug("modify and save <<<");
+        }
+
+        // Finally, we goto the default record, here could not await, make response time
+        // less to user
+        log.debug("goto the default record >>>");
+        SageActionService.doSageAct(auth, function, trans, sessionId,
+                SageActionHelper.set("B", targetMainField, 0, rcdNO), null);
+        log.debug("goto the default record <<<");
+
+        // return
+        return rtn;
+    }
+}
---
+++ src/main/java/com/da/sageassistantserver/webdav/AuthFilter.java
@@ -0,0 +1,292 @@
+/*****************************************************************************
+ * @Author                : Robert Huang<56649783@qq.com>                    *
+ * @CreatedDate           : 2024-07-04 09:39:40                              *
+ * @LastEditors           : Robert Huang<56649783@qq.com>                    *
+ * @LastEditDate          : 2024-07-11 09:32:05                              *
+ * @CopyRight             : Dedienne Aerospace China ZhuHai                  *
+ ****************************************************************************/
+
+package com.da.sageassistantserver.webdav;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.da.sageassistantserver.model.BusinessPartnerName;
+import com.da.sageassistantserver.model.User;
+import com.da.sageassistantserver.model.UserFunc;
+import com.da.sageassistantserver.service.LogService;
+import com.da.sageassistantserver.service.SageLoginService;
+import com.da.sageassistantserver.service.SupplierService;
+import com.da.sageassistantserver.service.UserFuncService;
+import com.da.sageassistantserver.service.UserService;
+import com.da.sageassistantserver.utils.Utils;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Component
+public class AuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserFuncService userFuncService;
+    @Autowired
+    SupplierService supplierService;
+    @Autowired
+    LogService logService;
+
+    final List<String> modifiableMethods = Arrays.asList("PUT", "POST", "PROPPATCH", "MKCOL", "MOVE");
+
+    @Override
+    protected void doFilterInternal(@SuppressWarnings("null") HttpServletRequest request,
+            @SuppressWarnings("null") HttpServletResponse response, @SuppressWarnings("null") FilterChain filterChain)
+            throws ServletException, IOException {
+        String userAgent = request.getHeader("User-Agent");
+        if (userAgent.contains("Microsoft-WebDAV-MiniRedir")) {
+            return;
+        }
+
+        // // trace request code block
+        // String path = getRelativePath(request);
+        // log.info("{} {}", request.getMethod(), path);
+
+        // Enumeration<String> headers = request.getHeaderNames();
+        // while (headers.hasMoreElements()) {
+        // String header = headers.nextElement();
+        // log.info("Header {}: {}", header, request.getHeader(header));
+        // }
+        // Enumeration<String> attributes = request.getAttributeNames();
+        // while (attributes.hasMoreElements()) {
+        // String attribute = attributes.nextElement();
+        // log.info("Attribute {}: {}", attribute, request.getAttribute(attribute));
+        // }
+        // Enumeration<String> parameters = request.getParameterNames();
+        // while (parameters.hasMoreElements()) {
+        // String parameter = parameters.nextElement();
+        // log.info("Parameter {}: {}", parameter, request.getParameter(parameter));
+        // }
+        if (auth(request, response)) {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    /**
+     * Check if the request is authorized.
+     *
+     * @note ❗❗❗ Window doesn't allow connect to no ssl webdav by default.. ❗❗❗
+     *       <p>
+     *       If Mapping disk or add web folder failed by default. To enable it,
+     *       you need to set:
+     *       <p>
+     *       \HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WebClient\Parameters
+     *       <p>
+     *       BasicAuthLevel to 2
+     *       <p>
+     *       Open CMD with Administrator privileges and run
+     *       <p>
+     *       net stop webclient
+     *       <p>
+     *       net start webclient
+     * @return
+     */
+    public boolean auth(HttpServletRequest httpReq, HttpServletResponse httpRes) {
+        // if (httpRes != null) {
+        // return false;
+        // }
+        HttpSession session = httpReq.getSession();
+
+        String authorization = httpReq.getHeader("Authorization");
+        if (authorization != null) {
+            String array[] = Utils.decodeBasicAuth(authorization).split(":");
+            String loginName = array[0].toLowerCase();
+            session.setAttribute("loginName", loginName);
+
+            // auth by Sage User
+            JSONObject loginResult = SageLoginService.doLogin(authorization);
+            if (loginResult.getBoolean("success")) {
+                // ignore cache login log, message is "Cache login success"
+                if (loginResult.getString("msg").equals("Login success")) {
+                    logService.addLog("LOGIN_SUCCESS", loginName);
+                }
+
+                User user = userService.getUserByLoginName(loginName);
+                if (user == null) {
+                    JSONObject profileResult = SageLoginService.getProfile(authorization);
+                    if (profileResult.getBoolean("success")) {
+                        JSONObject profile = profileResult.getJSONObject("profile");
+                        session.setAttribute("loginUser", profile.getString("userName"));
+
+                        userService.createUser(
+                                profile.getString("userId"), loginName,
+                                profile.getString("firstName"), profile.getString("lastName"),
+                                profile.getString("email"), profile.getString("language"));
+                    }
+                } else {
+                    session.setAttribute("loginUser", user.getFirst_name() + " " + user.getLast_name());
+                }
+                setBusinessPartner(httpReq, httpRes);
+                return authByPath(httpReq, httpRes);
+            }
+
+            return unauthorized(httpReq, httpRes);
+
+        } else { // no authorization
+            return unauthorized(httpReq, httpRes);
+        }
+    }
+
+    private void setBusinessPartner(HttpServletRequest httpReq, HttpServletResponse httpRes) {
+        String bpCode = httpReq.getParameter("bpCode");
+        if (bpCode != null) {
+            List<BusinessPartnerName> bpNames = supplierService.getBusinessPartnerByCode(bpCode);
+            if (bpNames.size() > 0) {
+                httpReq.getSession().setAttribute("bpNames", bpNames.get(0).getBPName());
+            }
+        }
+    }
+
+    private boolean authByPath(HttpServletRequest httpReq, HttpServletResponse httpRes) {
+        // String path = getRelativePath(httpReq);
+        String method = httpReq.getMethod();
+        String loginName = (String) httpReq.getSession().getAttribute("loginName");
+
+        List<UserFunc> userFuncs = userFuncService.getWebDavAccess(loginName);
+        if (userFuncs.size() == 0) {
+            boolean result = userFuncService.initWebDavAccessByLoginName(loginName);
+
+            if (result) {
+                logService.addLog("DOC_ACCESS_INIT_SUCCESS", loginName);
+                userFuncs = userFuncService.getWebDavAccess(loginName);
+            } else {
+                logService.addLog("DOC_ACCESS_INIT_FAILED", loginName);
+                return forbidden(httpReq, httpRes);
+            }
+        }
+
+        // try again
+        if (userFuncs.size() == 0) {
+            return forbidden(httpReq, httpRes);
+        } else {
+            boolean allowWrite = false;
+            boolean allowRead = false;
+            for (UserFunc userFunc : userFuncs) {
+                if (userFunc.getFunc_system().equals("WEBDAV") &&
+                        userFunc.getFunc_code().equals("WRITE") &&
+                        userFunc.isEnable()) {
+                    allowWrite = true;
+                } else if (userFunc.getFunc_system().equals("WEBDAV") &&
+                        userFunc.getFunc_code().equals("READ") &&
+                        userFunc.isEnable()) {
+                    allowRead = true;
+                }
+            }
+
+            if (allowWrite) {
+                return allow(httpReq, httpRes);
+            }
+            if (allowRead) {
+                return modifiableMethods.contains(method)
+                        ? forbidden(httpReq, httpRes)
+                        : allow(httpReq, httpRes);
+            }
+            // final return forbidden
+            return forbidden(httpReq, httpRes);
+        }
+    }
+
+    private boolean allow(HttpServletRequest req, HttpServletResponse res) {
+        String user = (String) req.getSession().getAttribute("loginName");
+        String userName = (String) req.getSession().getAttribute("loginUser");
+        String path = getRelativePath(req);
+        String remoteIP = getTrueRemoteIp(req);
+        if (!path.endsWith("/")) {
+            logService.addLog("DOC_ACCESS_SUCCESS", user, userName, path, remoteIP);
+        }
+
+        res.setStatus(200);
+        return true;
+    }
+
+    private boolean unauthorized(HttpServletRequest req, HttpServletResponse res) {
+        String user = (String) req.getSession().getAttribute("loginName");
+        String userName = (String) req.getSession().getAttribute("loginUser");
+        String path = getRelativePath(req);
+        String remoteIP = getTrueRemoteIp(req);
+        if (!path.endsWith("/")) {
+            logService.addLog("DOC_ACCESS_FAILED", user, userName, path, remoteIP);
+        }
+
+        res.setStatus(401);
+        res.setHeader("WWW-Authenticate", "Basic realm=\"DAV\"");
+        return true;
+    }
+
+    private boolean forbidden(HttpServletRequest req, HttpServletResponse res) {
+        String user = (String) req.getSession().getAttribute("loginName");
+        String userName = (String) req.getSession().getAttribute("loginUser");
+        String path = getRelativePath(req);
+        String remoteIP = getTrueRemoteIp(req);
+        if (!path.endsWith("/")) {
+            logService.addLog("DOC_ACCESS_FAILED", user, userName, path, remoteIP);
+        }
+
+        res.setStatus(403);
+        return false;
+    }
+
+    private String getTrueRemoteIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0) {
+            ip = request.getHeader("X-Real-IP");
+        }
+        if (ip == null || ip.length() == 0) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    /**
+     * This function is copied from WebDavServlet.getRelativePath()
+     * 
+     * @param request
+     * @return
+     */
+    protected String getRelativePath(HttpServletRequest request) {
+        String pathInfo;
+
+        if (request.getAttribute(RequestDispatcher.INCLUDE_REQUEST_URI) != null) {
+            // For includes, get the info from the attributes
+            pathInfo = (String) request.getAttribute(RequestDispatcher.INCLUDE_PATH_INFO);
+        } else {
+            pathInfo = request.getPathInfo();
+        }
+
+        StringBuilder result = new StringBuilder();
+        if (pathInfo != null) {
+            result.append(pathInfo);
+        }
+        if (result.length() == 0) {
+            result.append('/');
+        }
+
+        return result.toString();
+    }
+
+}
